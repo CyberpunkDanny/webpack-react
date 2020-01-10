ee41e265465e78166488abab2517811ff7cb6f41
Divyateja <divyateja.chepuru@bain.com>
2020-01-10 21:42:49 +0530

    React App configured with Webpack & Babel

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HTMLWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const htmlPlugin = new HTMLWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+})
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
+}
